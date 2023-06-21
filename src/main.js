a421daeba752ece065e68a0ce12af823e60c9e95
isaac-gordillo <isaac.gordillo@es.stroeer-labs.com>
2023-06-21 09:07:32 +0200

    rxjs playground

--- src/main.js
+++
@@ -1,7 +0,0 @@
-const numeroPi = 3.14159; // el numero Pi siempre será el mismo por lo que no tiene sentido cambiar el valor en un futuro;
-
-numeroPi = 24 // esto da error
-
-let numeroGoles = 0; // en cambio si almacenador algo que puede ir cambiando en el tiempo debemos permitir reasignar valores usando let
-
-numeroGoles = 1;
